a952470c91bbce7b2532f9bafe924f99ec79d2f8
ZZBAE <jebyun2@gmail.com>
2022-09-14 09:19:20 +0900

    feat: 프로젝트 생성

---
+++ YaTon Project/YaTon Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YaTon Project
+//
+//  Created by 변재은 on 2022/09/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YaTon Project/YaTon Project/YaTon_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaTon_ProjectApp.swift
+//  YaTon Project
+//
+//  Created by 변재은 on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct YaTon_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
