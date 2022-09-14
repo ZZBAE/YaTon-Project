6dfb1696958b6e101d1d79c6b2942fdd6e8664f0
ZZBAE <jebyun2@gmail.com>
2022-09-14 11:44:17 +0900

    feat: model인 MountainEquipment 생성

---
+++ YaTon Project/YaTon Project/Model/MountainEquipment.swift
@@ -0,0 +1,35 @@
+//
+//  Model.swift
+//  YaTon Project
+//
+//  Created by 재재, 그루트 on 2022/09/14.
+//
+
+import Foundation
+
+import Foundation
+import SwiftUI
+
+struct MountainEquipment: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var price: Int
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case northFace = "NorthFace"
+        case discovery = "Discovery"
+        case K2 = "K2"
+        case nationalGeographic = "National Geographic"
+        case blackYak = "BlackYak"
+        case nepa = "Nepa"
+        case eider = "Eider"
+        case kolongSport = "Kolong Sport"
+        case columbia = "Columbia"
+        case millet = "Millet"
+    }
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+}
